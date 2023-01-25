62c20744d4bf09d899796b028657e5d769c9eae2
VladanLalic23 <104385241+VladanLalic23@users.noreply.github.com>
2023-01-25 21:27:31 +0100

    web config update

--- src/main/java/ftn/IsaProjekat/config/WebConfig.java
+++ src/main/java/ftn/IsaProjekat/config/WebConfig.java
@@ -9,11 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 
-	//Enable CORS for avoiding @CrossOrigin annotation on every controller in application
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:7070").
-        allowedMethods("GET","POST","PUT","DELETE","HEAD").allowedHeaders("*");
-
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("GET","POST","PUT","DELETE","HEAD")
+        .exposedHeaders("Authorization");
     }
 }
--- src/main/java/ftn/IsaProjekat/config/WebSecurityConfig.java
+++ src/main/java/ftn/IsaProjekat/config/WebSecurityConfig.java
@@ -6,15 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import ftn.IsaProjekat.security.TokenUtils;
@@ -24,65 +22,74 @@ import ftn.IsaProjekat.service.UserService;
 
 
 @Configuration
-@EnableWebSecurity
+//Enable annotation "@Pre*" & "@Post*" which are going to check authorization for every method access
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
-
+	//Implementing of PasswordEncoder with BCrypt hashing function
+	//BCrypt by default does 10 rounds of forwarded value hashing.
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
+	//Service which is used for reading data about application users
 	@Autowired
-	private UserService jwtUserService;
+	private UserService jwtUserDetailsService;
 	
+	//Handler for returning 401 when client with incorrect username and password tries to access the resource
 	@Autowired
 	private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-
-
-
+	
+	//We register an authentication manager who is going to do user authentication for us
 	@Bean
-	AuthenticationManager authenticationManager(
-			AuthenticationConfiguration authenticationConfiguration) throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 	
-
-	
-	// Definisemo nacin autentifikacije
+	//We define instructions for the authentication manager which service uses to extract data about the user who wants to be authenticated,
+    //as well as through which encoder is going to pass the password received from the client in the request to compare the adequate hash obtained as a result of the bcrypt algorithm with the one in the database
+	//We dont have plain password in the database
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(jwtUserService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
 	}
 	
+	//We inject an implementation from the TokenUtils class so we can use its methods to work with JWT in the TokenAuthenticationFilter
 	@Autowired
 	private TokenUtils tokenUtils;
-	// Definisemo prava pristupa odredjenim URL-ovima
-	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	
+	//ACCES TO SPECIFIC URL'S
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
+        	//REST application
         	.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         	
-			.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
-        	
+        	//Send a 401 error for every unathorized request
+        	.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+        	//Allow all users to access defined paths
         	.authorizeRequests().antMatchers("/auth/**").permitAll()
+			.antMatchers("/clinic/all").permitAll()
 
+        	//For every other request the user must be authenticated 
         	.anyRequest().authenticated().and()
         	
+        	//For development purposes include configuration for CORS from the WebConfig class
         	.cors().and()
         	
-        	.addFilterBefore(new TokenAuthenticationFilter(tokenUtils, jwtUserService),
+        	//Insert custom filter TokenAuthenticationFilter to check JWT tokens instead of pure username and password (performed by BasicAuthenticationFilter)
+        	.addFilterBefore(new TokenAuthenticationFilter(tokenUtils, jwtUserDetailsService),
         			BasicAuthenticationFilter.class);
- 
+        //Due to simplicity of the example	
         http.csrf().disable();	 
-
-		return http.build();
     }
-	 // Generalna bezbednost aplikacije
-	@Bean
-    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
-		
-		return (web)-> web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "/favicon.ico", "/**/*.html",			"/**/*.css", "/**/*.js");
 	
+	//GENERAL SECURITY OF THE APPLICATION
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+		
+		web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "/favicon.ico", "/**/*.html",
+				"/**/*.css", "/**/*.js");
 	}
 }
